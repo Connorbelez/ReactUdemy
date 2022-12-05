fbdd51ca3e7ab24593cdf884c3420cd86a95ea97
Connorbelez <connor.belez@gmail.com>
2022-12-05 00:51:34 -0500

    commit message

--- A2/src/components/ExpensesComps/ExpenseItems.js
+++ A2/src/components/ExpensesComps/ExpenseItems.js
@@ -19,6 +19,7 @@ export function ExpenseItems (props)
     return (
         <div> 
  
+ 
             <Card className="expenses">
             <ExpensesFilter value={selectedYear} onFilterChange={addFilterHandler}/>
                 {/* <ExpenseItem
